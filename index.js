c6bf5404b7701bc13c12c408082e92205a144248
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-05-12 23:33:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const append = ["Milo", "Otis", "Garfield"];
-const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
 const append = ["Milo", "Otis", "Garfield"];
 const append = ["Milo", "Otis", "Garfield"];
